19363022de3d2abb1d588064d588a5284f9f2ee7
Bloodacote <bloodacote@gmail.com>
2023-12-13 16:26:53 +0300

    b17-simple-php-compiler
    Writing a simple function of insert JS code in PHP compiler

--- sample/_wafle/compiler.php
+++ sample/_wafle/compiler.php
@@ -1,5 +1,26 @@
 <?php
 
+/* - - - - - - - - - - - - - -
+	PHP компилятор для JS-кода
+- - - - - - - - - - - - - - */
+
+// Установка корневых папок
+$site_dir = $_SERVER["DOCUMENT_ROOT"];
+$engine_dir = __DIR__ . "\jsengine";
+
+//echo $engine_dir;
+
+// Загрузка контента JS-скрипта
+function insertScript($script_path) {
+	global $engine_dir;
+
+	$script_path = $engine_dir . $script_path;
+	$script_content = file_get_contents($script_path);
+	echo nl2br($script_content);
+}
+
 echo "// WAFLE JS-engine compiler \n";
+
+insertScript("/run.js");
 
 ?>
