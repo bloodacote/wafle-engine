bbea0a4f3635fe0e3e570793f1f6c05e4b05c75f
Bloodacote <bloodacote@gmail.com>
2023-12-15 22:46:41 +0300

    c3-partos-posu-elem
    Creating posuElem() and adding function of init()

--- sample/_wafle/js-engine/modules/partos-ultra.js
+++ sample/_wafle/js-engine/modules/partos-ultra.js
@@ -1,7 +1,26 @@
 
 
+// Класс модифицированного элемента
+class posuElem {
+	constructor(selector) {
+		this.selector = selector;
+		this.elem = null;
+		this.place = null;
+
+		this.init();
+	}
+
+	// Инициализация / Устанвока нового селектора
+	init(selector = this.selector) {
+		this.selector = selector;
+		this.elem = document.querySelector(this.selector);
+		this.place = this.elem.parentElement;
+	}
+}
+
+
 // Функция получает элемент
 function el(elemSelector) {
-	var elem = document.querySelector(elemSelector);
+	var elem = new posuElem(elemSelector);
 	return elem;
 }
